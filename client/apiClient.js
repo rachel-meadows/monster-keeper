aead6fa5c23774738c333accdee942c251000206
hongxukang <hong.xkang@gmail.com>
2022-04-15 11:32:44 +1200

    completed mint functionality

--- client/apiClient.js
+++ client/apiClient.js
@@ -2,9 +2,8 @@ import request from 'superagent'
 
 const monsterUrl = '/api/v1/monsters' // ONly a suggestion, need to agree with backend
 
+//Returns a string for the src of the image
 export function getImage() {
+  // return request.get(monsterUrl)
   return Promise.resolve('Placeholder URL for Image')
 }
-
-// const image =
-//   'https://st.depositphotos.com/1787196/2301/i/950/depositphotos_23016936-stock-photo-3d-cartoon-green-monster.jpg'
--- client/components/MintBox.jsx
+++ client/components/MintBox.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { getImage } from '../apiClient.js'
 
 function MintBox() {
   //Get image from API
-  
+  const [image, setImage] = useState('')
 
   const health = Math.floor((Math.random() * 100) + 1)
   const damage = Math.floor((Math.random() * 100) + 1)
-  let image = ''
-
-  getImage().then((url) => image = url).catch(err => console.error(err))
-
-  console.log(image)
+ 
+  getImage().then((str) => setImage(str)).catch(err => console.error(err))
 
   return (
     <div>
